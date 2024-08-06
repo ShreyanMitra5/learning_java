eb6b44bd7e3f7a4955a0ab22aa6c18d9619313c5
ShreyanMitra5 <shreyan.mitra09@gmail.com>
2024-08-05 22:14:52 -0700

    Update 7

---
+++ learning_java/Switch Statement/switch_statements.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class switch_statements{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a command: ");
+        String text = input.nextLine();
+
+        switch(text){
+        case "start":
+            System.out.println("Machine Started!");
+            break;
+        case "stop":
+            System.out.println("Machine Stopped.");
+            break;
+        //Default Command for any other testcase    
+        default:
+            System.out.println("Command not recognized");
+        }
+    }    
+}
